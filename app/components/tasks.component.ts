38334876ea134160bb8343b536cc75f7cab21a4b
Geethanga Amarasinghe <geethanga.amarasinghe@pearson.com>
2016-09-25 13:21:34 +0530

    Using OnInit to initialize the service

--- app/components/tasks.component.ts
+++ app/components/tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {TaskService} from '../services/taskservice';
 import {AlertComponent} from 'ng2-bootstrap/ng2-bootstrap';
 import {CORE_DIRECTIVES} from '@angular/common';
@@ -14,11 +14,16 @@ import {Task} from '../models/Task';
       styles: [".red {color: red}", ".blue { color: blue}"]
     })
 
-    export class TasksComponent {
+    export class TasksComponent implements OnInit {
       constructor(public taskService: TaskService){}
       toggle: boolean = false ;
       taskName: string;
       assigneeName: string;
+      tasks : Array<Task>;
+
+      ngOnInit():void {
+        this.initializeTasks();
+      }
 
       addTask(){
         this.taskService.insert(this.taskName, this.assigneeName);
@@ -26,7 +31,9 @@ import {Task} from '../models/Task';
         this.assigneeName = "";
       }
 
-      myTasks : Array<Task> = this.taskService.getTasks();
+      initializeTasks(){
+        this.tasks = this.taskService.getTasks();
+      }
     } 
 
     
